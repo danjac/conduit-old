93f753fab7239347f528d041e05d26a465a5a0c1
Dan Jacob <danjac2018@gmail.com>
2020-11-21 22:09:41 +0200

    Follow/unfollow

--- conduit/users/tests/test_views.py
+++ conduit/users/tests/test_views.py
@@ -16,3 +16,22 @@ class TestUserDetailIndex:
         response = client.get(reverse("users:detail", args=[user.username]))
         assert response.status_code == 200
         assert len(response.context["articles"].object_list) == 6
+
+
+class TestUserFollow:
+    def test_post(self, client, login_user, user):
+        response = client.post(reverse("users:follow", args=[user.id]))
+        assert response.url == user.get_absolute_url()
+        assert user in login_user.follows.all()
+
+    def test_post_follow_self(self, client, login_user):
+        response = client.post(reverse("users:follow", args=[login_user.id]))
+        assert response.status_code == 404
+
+
+class TestUserUnfollow:
+    def test_post(self, client, login_user, user):
+        login_user.follows.add(user)
+        response = client.post(reverse("users:unfollow", args=[user.id]))
+        assert response.url == user.get_absolute_url()
+        assert user not in login_user.follows.all()
