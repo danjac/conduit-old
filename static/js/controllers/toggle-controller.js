938473a4c46f6886553d48f43b77104b700d5bbe
Dan Jacob <danjac2018@gmail.com>
2020-12-05 23:31:10 +0200

    Stimulus2.0 upgrade

--- static/js/controllers/toggle-controller.js
+++ static/js/controllers/toggle-controller.js
@@ -4,17 +4,8 @@ export default class extends Controller {
   static targets = ['item'];
 
   toggle() {
-    // toggle DOM element
-    if (this.hasItemTargets) {
-      this.itemTargets.forEach((item) => {
-        item.classList.toggle(this.toggleClass);
-      });
-    } else {
-      this.element.classList.toggle(this.toggleClass);
-    }
-  }
-
-  get toggleClass() {
-    return this.data.get('class') || 'hidden';
+    this.itemTargets.forEach((item) => {
+      item.classList.toggle('hidden');
+    });
   }
 }
